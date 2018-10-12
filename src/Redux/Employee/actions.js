08541b66cd626425aab7e03deef68d848030286d
Jayaram_dev <jayaramvenkatanarayanan@live.com>
2018-10-12 14:49:06 +0530

    save redux point added

--- src/Redux/Employee/actions.js
+++ src/Redux/Employee/actions.js
@@ -26,3 +26,29 @@ export function fetchEmployee() {
     }
 }
 
+//save Employee
+//get all employee
+export const SAVE_EMPLOYEE = 'SAVE_EMPLOYEE'
+
+export function SaveEmployeeSuccess(employee) {
+    return {type: SAVE_EMPLOYEE, employee}
+}
+
+export function SaveEmployee() {
+    let url = common.ApiUrl + 'getAllEmp'
+    return dispatch => {
+        Axios
+            .get(url, {
+                headers: {
+                    'x-auth': common.token
+                }
+            })
+            .then(function (response) {
+                dispatch(SaveEmployeeSuccess(response.data))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+}
+
--- src/Redux/Employee/employee_reducers.js
+++ src/Redux/Employee/employee_reducers.js
@@ -1,8 +1,10 @@
-import {SET_EMPLOYEE} from './actions'
+import {SET_EMPLOYEE,SAVE_EMPLOYEE} from './actions'
 export default(state = [], action = {}) => {
     switch (action.type) {
         case SET_EMPLOYEE:
             return action.employee
+        case SAVE_EMPLOYEE:
+            return [...state ,action.employee]
         default:
             return state
     }
